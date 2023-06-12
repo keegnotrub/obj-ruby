fa18bbd14a9df1cbc040b6973234d9613bf77525
Ryan Krug <ryan.krug@thoughtbot.com>
2023-06-12 06:11:43 -0500

    Run standardrb

--- spec/foundation/rect_spec.rb
+++ spec/foundation/rect_spec.rb
@@ -3,7 +3,7 @@ require "obj_ruby/foundation"
 
 describe ObjRuby::NSRect do
   it "can create an instance" do
-    rect = ObjRuby::NSMakeRect(1,2,3,4)
+    rect = ObjRuby::NSMakeRect(1, 2, 3, 4)
 
     expect(rect).not_to be_nil
     expect(rect).to be_a described_class

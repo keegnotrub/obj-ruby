f7ce8d1ee218fe79e5b0eeb49f9b64e66b235cc2
Ryan Krug <ryan.krug@thoughtbot.com>
2023-06-14 06:46:40 -0500

    Test dict/hash equality

--- spec/foundation/dictionary_spec.rb
+++ spec/foundation/dictionary_spec.rb
@@ -12,15 +12,20 @@ describe ObjRuby::NSDictionary do
   it "can call instance methods" do
     dict = described_class.dictionaryWithObject_forKey("value", "key")
 
-    expect(dict.objectForKey("key")).to eq "value"
-    expect(dict.objectForKey("not_key")).to be_nil
+    expect(dict.objectForKey(:key)).to eq "value"
+    expect(dict.objectForKey(:not_key)).to be_nil
   end
 
   it "can receive a Ruby hash" do
-    dict = described_class.dictionaryWithDictionary(key: "value")
+    dict = described_class.dictionaryWithDictionary(key1: "value1", key2: "value2")
 
-    expect(dict.objectForKey("key")).to eq "value"
-    expect(dict.objectForKey("not_key")).to be_nil
+    manual_dict = described_class.dictionaryWithObjectsAndKeys("value1", :key1, "value2", :key2, nil)
+
+    expect(dict.count).to eq 2
+    expect(dict.objectForKey(:key1)).to eq "value1"
+    expect(dict.objectForKey(:key2)).to eq "value2"
+    expect(dict.objectForKey(:not_key)).to be_nil
+    expect(dict.isEqualToDictionary(manual_dict)).to be true
   end
 
   it "can be transformed into a Ruby hash" do

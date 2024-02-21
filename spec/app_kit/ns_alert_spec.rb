b92847f08d27f4f8ba449ba8b3975b12b1a19f7f
Ryan Krug <ryan.krug@thoughtbot.com>
2024-02-21 09:01:36 -0600

    Adjust `objc_msgSend` for structs and floats
    This may not be 100% correct, as there are some ABI differences
    between Intel and ARM on when exactly to use `objc_msgSend_stret` and
    `objc_msgSend_fpret` but it brings us a bit closer to reality.
    
    Additionally, we need to modify our need to always expose clases by
    their public name, as some classes like `NSAttributedString` have
    `NSConcrete` versions that are returned in order to implement certain
    methods. We see this in `NSAttributedString#initWithString` For now,
    we check the internal class for `NSConcrete*` and allow that to go
    through.

--- spec/app_kit/ns_alert_spec.rb
+++ spec/app_kit/ns_alert_spec.rb
@@ -3,9 +3,9 @@ require "obj_ruby/app_kit"
 
 describe ObjRuby::NSAlert do
   it "can create an instance" do
-    array = described_class.new
+    alert = described_class.new
 
-    expect(array).not_to be_nil
-    expect(array).to be_a described_class
+    expect(alert).not_to be_nil
+    expect(alert).to be_a described_class
   end
 end

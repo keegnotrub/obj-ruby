82494216b412687add8fc52540563308cded33d9
Ryan Krug <ryank@kit.com>
2025-05-24 05:30:26 -0500

    More specs for ObjRuby.import

--- spec/obj_ruby_spec.rb
+++ spec/obj_ruby_spec.rb
@@ -3,20 +3,39 @@
 require "spec_helper"
 
 RSpec.describe ObjRuby do
-  it "defines Foundation Objective-C constants" do
-    expect(described_class.const_get(:NSNotFound)).to eq (2**63) - 1
-    expect(described_class.const_get(:NSOrderedSame)).to eq 0
-    expect(described_class.const_get(:NSASCIIStringEncoding)).to eq 1
-    expect(described_class.const_get(:NSWeekDayNameArray)).to eq "NSWeekDayNameArray"
-    expect(described_class.const_get(:NSZombieEnabled)).to be false
-  end
+  describe ".import" do
+    it "returns false for already loaded frameworks" do
+      # spec_helper loads these
+      expect(described_class.import("Foundation")).to be false
+      expect(described_class.import("CoreData")).to be false
+      expect(described_class.import("AppKit")).to be false
+    end
 
-  it "defines AppKit Objective-C constants" do
-    expect(described_class.const_get(:NSOKButton)).to eq 1
-    expect(described_class.const_get(:NSCancelButton)).to eq 0
-  end
+    it "returns true for frameworks not yet loaded" do
+      expect(described_class.import("WebKit")).to be true
+    end
 
-  it "defines CoreData Objective-C constants" do
-    expect(described_class.const_get(:NSAffectedObjectsErrorKey)).to eq "NSAffectedObjectsErrorKey"
+    it "raises an error for unknown frameworks" do
+      expect do
+        described_class.import("NotAFramework")
+      end.to raise_error(LoadError)
+    end
+
+    it "defines Foundation Objective-C constants" do
+      expect(described_class.const_get(:NSNotFound)).to eq (2**63) - 1
+      expect(described_class.const_get(:NSOrderedSame)).to eq 0
+      expect(described_class.const_get(:NSASCIIStringEncoding)).to eq 1
+      expect(described_class.const_get(:NSWeekDayNameArray)).to eq "NSWeekDayNameArray"
+      expect(described_class.const_get(:NSZombieEnabled)).to be false
+    end
+
+    it "defines CoreData Objective-C constants" do
+      expect(described_class.const_get(:NSAffectedObjectsErrorKey)).to eq "NSAffectedObjectsErrorKey"
+    end
+
+    it "defines AppKit Objective-C constants" do
+      expect(described_class.const_get(:NSOKButton)).to eq 1
+      expect(described_class.const_get(:NSCancelButton)).to eq 0
+    end
   end
 end

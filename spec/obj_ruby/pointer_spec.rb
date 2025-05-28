70891c0e89aeea9e789e380fc5c41138b822c8da
Ryan Krug <ryank@kit.com>
2025-05-28 06:04:46 -0500

    Add test

---
+++ spec/obj_ruby/pointer_spec.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe ObjRuby::Pointer do
+  describe "creating an instance of pointer" do
+    it "can create an instance pointing to single type encoding" do
+      ptr = described_class.new(:double)
+
+      expect(ptr).not_to be_nil
+      expect(ptr).to be_a described_class
+    end
+
+    it "can create an instance pointing to an array of a type encoding" do
+      ptr = described_class.new(:double, 10)
+
+      expect(ptr).not_to be_nil
+      expect(ptr).to be_a described_class
+    end
+
+    it "raises an error if not provided the type encoding" do
+      expect do
+        described_class.new
+      end.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "when passing pointer as a param" do
+    it "sets NSError data for an object pointer" do
+      error_ptr = described_class.new(:object)
+
+      result = ObjRuby::NSData.dataWithContentsOfFile_options_error("not-a-path", 0, error_ptr)
+
+      expect(result).to be_nil
+
+      error = error_ptr.at(0)
+
+      expect(error).not_to be_nil
+      expect(error).to be_a ObjRuby::NSError
+      expect(error.localizedDescription).to eq(
+        "The file “not-a-path” couldn’t be opened because there is no such file."
+      )
+      expect(error_ptr[0]).to eq error
+
+      array = error_ptr.slice(0, 1)
+
+      expect(array[0]).to eq error
+      expect(error_ptr[0, 1]).to eq array
+    end
+
+    it "sets CGFloat data for a value pointers" do
+      red = described_class.new(:double)
+      green = described_class.new(:double)
+      blue = described_class.new(:double)
+      alpha = described_class.new(:double)
+
+      color = ObjRuby::NSColor.redColor
+
+      color.getRed_green_blue_alpha(red, green, blue, alpha)
+
+      expect(red[0]).to eq(1.0)
+      expect(green[0]).to eq(0.0)
+      expect(blue[0]).to eq(0.0)
+      expect(alpha[0]).to eq(1.0)
+    end
+
+    # it "sets CGFloat array data for an array of value pointers" do
+    #   import Accelerate
+    #
+    #   let a: [Float] = [1, 2, 3, 4]
+    #   let b: [Float] = [0.5, 0.25, 0.125, 0.0625]
+    #   var result: [Float] = [0, 0, 0, 0]
+    #
+    #   vDSP_vadd(a, 1, b, 1, &result, 1, 4)
+    #
+    #   result now contains [1.5, 2.25, 3.125, 4.0625]
+    # end
+  end
+end
